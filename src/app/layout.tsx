2949530d22e786bab615987de573621cb1fc3527
Yush yetunde <yusufyetunde13@gmail.com>
2024-06-29 16:31:59 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers>
           <Navbar />
-          <LoginModal/>
+          {/* <LoginModal/> */}
 
           {children}
         </Providers>
